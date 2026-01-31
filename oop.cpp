dd7eddc556ed39d2606b86b0ff849e2eaa40ebb5
parthpishte <parth.pishte21@vit.edu>
2026-01-31 16:46:23 +0530

    add acess modifiers

--- oop.cpp
+++ oop.cpp
@@ -1,139 +1,167 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class default_class{
-    
-    int a;
-    int b;                            //by default is it private except its constructor
-    void display(){
-        cout<<"hello parth"<<endl;
-    }
-};
-class public_class{
-    public:
-    int a;                           //use . for ref to an obj and :: ref to a class itself
-    int b;
-    void display(){
-        cout<<"hello parth"<<endl;
-    }
-};
-class para_constructor_class{
-    
-    int a;
-    int b;
-    public:
-    para_constructor_class(int a,int b){
-        this->a=a;
-        this->b=b;
-    }
-    void display(){
-        cout<<"a is :"<<a<<endl;
-         cout<<"b is :"<<b<<endl;
-    }
+class default_class
+{
 
+    int a;
+    int b; // by default is it private except its constructor
+    void display()
+    {
+        cout << "hello parth" << endl;
+    }
 };
-class static_class{
-    public:
+class public_class
+{
+public:
+    int a; // use . for ref to an obj and :: ref to a class itself
+    int b;
+    void display()
+    {
+        cout << "hello parth" << endl;
+    }
+};
+class para_constructor_class
+{
+
+    int a;
+    int b;
+
+public:
+    para_constructor_class(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+    void display()
+    {
+        cout << "a is :" << a << endl;
+        cout << "b is :" << b << endl;
+    }
+};
+class static_class
+{
+public:
     static int a;
-    static void show(){  // static data member are declared outside the class usint type class_name::var_name
-        cout<<"hello for static class"<<endl;
+    static void show()
+    { // static data member are declared outside the class usint type class_name::var_name
+        cout << "hello for static class" << endl;
     }
 };
-    int static_class::a=10;
+int static_class::a = 10;
 
-class encapsulation_class{
+class encapsulation_class
+{
     int a;
-    public:
-    int get_a(){
+
+public:
+    int get_a()
+    {
         return this->a;
     }
-    void set_a(int a){
-        this->a=a;
-        cout<<this->a;
+    void set_a(int a)
+    {
+        this->a = a;
+        cout << this->a;
     }
-
 };
-class method_overloading{
-        public:
-        void display(){
-            cout<<"hello";
-        }
-        void display(string name){
-            cout<<"hello"<<name;
-        }
+class method_overloading
+{
+public:
+    void display()
+    {
+        cout << "hello";
+    }
+    void display(string name)
+    {
+        cout << "hello" << name;
+    }
 };
-class Parent_class{
-    public:
-    const int a=10;
+class Parent_class
+{
+public:
+    const int a = 10;
     int b;
-    void display(){
-        cout<<"hello from display";
-    }
-
-
-};
-class child_class: public Parent_class{    // to inheeit a claas use    child class:access_tpye class_nbame
-        int c;                              //excutable code only allowd insied mehtods or construcuto
-                                            // to access data memeber and mehtods use claas_name::member
-        //cout<<a<<;
-        public:
-        child_class(){
-                 cout<<Parent_class::a;
-                 Parent_class::display();
-        }
-};
-class method_overrriding{                               //method_overrriding *p;
-                                                        //derived d;
-                                                        //p=&d;
-                                                        //(*p).show();
-        public:
-         virtual void show(){
-            cout<<"hello from child class";
-        }
-};
-class derived:public method_overrriding{
-    public:
-    void show(){
-        cout<<"hello from derived";
-        
+    void display()
+    {
+        cout << "hello from display";
     }
 };
-class multiple_inheritance_A{
+class child_class : public Parent_class
+{          // to inheeit a claas use    child class:access_tpye class_nbame
+    int c; // excutable code only allowd insied mehtods or construcuto
+           //  to access data memeber and mehtods use claas_name::member
+    // cout<<a<<;
+public:
+    child_class()
+    {
+        cout << Parent_class::a;
+        Parent_class::display();
+    }
+};
+class method_overrriding
+{ // method_overrriding *p;
+  // derived d;
+  // p=&d;
+  //(*p).show();
+public:
+    virtual void show()
+    {
+        cout << "hello from child class";
+    }
+};
+class derived : public method_overrriding
+{
+public:
+    void show()
+    {
+        cout << "hello from derived";
+    }
+};
+class multiple_inheritance_A
+{
 
-    public:
+public:
     int a;
-    void show(){
-        cout<<"greetings";
+    void show()
+    {
+        cout << "greetings";
     };
 };
-class multiple_inheritance_B{
-    public:
+class multiple_inheritance_B
+{
+public:
     int a;
-    void show(){
+    void show()
+    {
         "helo";
     };
-
 };
-class multiple_inheritance_C:public multiple_inheritance_A,public multiple_inheritance_B{
-    public:
-    void display(){
-        cout<<"hello there!"<<endl;
+class multiple_inheritance_C : public multiple_inheritance_A, public multiple_inheritance_B
+{
+public:
+    void display()
+    {
+        cout << "hello there!" << endl;
         multiple_inheritance_A::show();
     }
-    
 };
 
-int main(){
+int main()
+{
     default_class A1;
     public_class A2;
-    para_constructor_class A3(5,3);
+    para_constructor_class A3(5, 3);
     encapsulation_class A4;
     static_class::show();
     method_overloading A5;
     child_class A6;
     multiple_inheritance_C A7;
-    
-  
-    
-    
+
     return 0;
 }
+
+
+//access modifier
+//1.public->accessible anywhere
+//2.protected->same class and child class
+//3.private ->same class only
